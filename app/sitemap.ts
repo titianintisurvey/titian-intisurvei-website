93651f58c45f514a47342376f4b413f8aedeb56b
titianintisurvey <titianintisurvey@gmail.com>
2026-03-16 08:57:46 +0700

    Create sitemap.ts

---
+++ app/sitemap.ts
@@ -0,0 +1,12 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://titianintisurvei.com",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://titianintisurvei.com/services",
+      lastModified: new Date(),
+    }
+  ]
+}
